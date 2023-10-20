a713161a09033adc295d2bc3dbbc17e818428abd
valdis <valdisdot@gmail.com>
2023-10-20 18:02:43 +0300

    create class QRGenerator

---
+++ src/main/java/com/valdisdot/sandbox/generate/QRGenerator.java
@@ -0,0 +1,25 @@
+package com.valdisdot.sandbox.generate;
+
+public class QRGenerator {
+    /*
+        constructor params:
+        Function<byte[], Integer> checksumFunction;
+        UnaryOperator<byte[], String> encryptFunction;
+
+        setPrefix(String prefix)
+        setQRCodeType(char type)
+        setCashRegister(int cashRegisterNumber)
+        setSalesPoint(int salesPoint)
+        setPseudoRandom(int pseudoRandom)
+        setUnixTimeSecond(long seconds)
+        setValidityDays(int days)
+        setPrice(int price)
+        setPLUCell(int cell)
+        setPLURange(int range)
+        setPostfix(String postfix)
+
+        getQRCodeAsString()
+        getQRCodeAsImage()
+
+    * */
+}

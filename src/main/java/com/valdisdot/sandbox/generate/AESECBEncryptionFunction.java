472e9e91b740924ba43fc3e37b14af021cbe3039
valdisdot <valdisdot@gmail.com>
2023-10-21 11:42:34 +0300

    make function more abstract, process byte[]

--- src/main/java/com/valdisdot/sandbox/generate/AESECBEncryptionFunction.java
+++ src/main/java/com/valdisdot/sandbox/generate/AESECBEncryptionFunction.java
@@ -2,29 +2,28 @@ package com.valdisdot.sandbox.generate;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
 import java.util.function.UnaryOperator;
 
-public class AESECBEncryptionFunction implements UnaryOperator<String> {
+public class AESECBEncryptionFunction implements UnaryOperator<byte[]> {
     // 16-character key for AES-128
-    private String secretKey;
+    private final String secretKey;
 
-    public AESECBEncryptionFunction(String secretKey){
+    public AESECBEncryptionFunction(String secretKey) {
         this.secretKey = secretKey;
     }
 
     @Override
-    public String apply(String plaintext) {
+    public byte[] apply(byte[] data) {
         try {
-            // Create a secret key specification
+            //create a secret key specification
             SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-            // Create a cipher object and set it to encryption mode with ECB
+            //create a cipher object and set it to encryption mode with ECB
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            // Encrypt, convert and return the encrypted bytes to a base64-encoded string
-            return Base64.getEncoder().encodeToString(cipher.doFinal(plaintext.getBytes("UTF-8")));
-        } catch (Exception e){
-            return "";
+            //encrypt and return data
+            return cipher.doFinal(data);
+        } catch (Exception e) {
+            return new byte[0];
         }
     }
 }
